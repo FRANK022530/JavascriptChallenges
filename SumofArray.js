ab4701c03b66a26587c9025b60cbf44c1e786f90
FRANK022530 <franklin.delosreyes@cvsu.edu.ph>
2024-09-07 14:34:49 +0800

    3 Challenges

---
+++ SumofArray.js
@@ -0,0 +1,15 @@
+
+function summation (arraynumber){
+    let sum = 0;
+
+    for (number of arraynumber){
+        sum = sum + number;
+    }
+
+    return sum;
+}
+
+
+
+array = [1,2,3,4,5,6,7,8]
+console.log(summation(array))
---
+++ Sumuptonumber.js
@@ -0,0 +1,13 @@
+
+function SumuptoNumber(number){
+    let sum = 0;
+
+    for (let i = 0; i <= number; i++){
+        sum = sum + i
+    }
+    return sum;
+
+
+}
+
+console.log(SumuptoNumber(5))
